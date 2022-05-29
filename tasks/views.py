32a5f84c8df4853f2860ad23e898e08f02e2d957
Junus Ergin <junus@developerakademie.com>
2022-05-29 11:47:46 +0200

    creating a task works

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 import django
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
+
+from userinfo.models import Profile
 from .models import Task
 from django.http import JsonResponse
 from django.core import serializers
@@ -18,6 +20,22 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        kwargs = self.kwargs
+        # kwargs = self.kwargs
         # print(kwargs)
-        return Task.objects.filter(group=123)
+        profile = Profile.objects.get(user=self.request.user)
+        return Task.objects.filter(group=profile.group)
+
+    def create(self, request, *args, **kwargs):
+        print('POST CALLED')
+        profile = Profile.objects.get(user=self.request.user)
+        due_date = datetime.datetime.strptime(request.POST['due_date'], "%Y-%m-%d")
+        task = Task.objects.create(
+                group=profile.group,
+                title=request.POST['title'],
+                description=request.POST['description'],
+                category=request.POST['category'],
+                urgency=request.POST['urgency'],
+                due_date=due_date,
+            )
+        serialized_obj = serializers.serialize('json', [ task, ])
+        return HttpResponse(serialized_obj[1:-1], content_type='application/json')
